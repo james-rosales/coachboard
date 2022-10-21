11c90c8a5bbeb4674b184797a92a6b7dad0ff88a
James Carlo <r_jamescarlo31@yahoo.com>
2022-10-21 14:58:48 +0800

    james oct 21

---
+++ lib/pages/coach_board/view/coach_board_form.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CoachBoardForm extends StatelessWidget {
+  const CoachBoardForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
