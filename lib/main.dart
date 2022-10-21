bdd95948153c77eb0382985d948fdc69abfd21e7
James Carlo <r_jamescarlo31@yahoo.com>
2022-10-21 15:00:42 +0800

    james oct 21

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coachboard/pages/coach_board/view/coach_board_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(
+      home: const CoachBoardPage(
         title: 'Coach Board',
       ),
     );
--- lib/pages/coach_board/view/coach_board_page.dart
+++ lib/pages/coach_board/view/coach_board_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CoachBoardPage extends StatelessWidget {
-  const CoachBoardPage({super.key});
+  const CoachBoardPage({super.key, required String title});
 
   @override
   Widget build(BuildContext context) {
